6fa6afc0d4d805c0d694160446b7909d6c948d99
Komenpurity <jepkorirpurity8@gmail.com>
2023-03-23 09:43:45 +0300

    display todo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
+// submitting a form
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", 
+  (e) => {
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);  
+  })
 });
+
+
+function handleToDo(task){
+  let li = document.createElement("li")
+  li.textContent = task
+
+  let btn = document.createElement("button")
+  btn.textContent = "X"
+  li.appendChild(btn) 
+
+  document.getElementById("tasks").appendChild(li) 
+}
